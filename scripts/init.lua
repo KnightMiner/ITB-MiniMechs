0c14d458b0a8ded0fc4973f631e5741e9ddbd63c
KnightMiner <knightminer4@gmail.com>
2022-07-19 01:49:26 -0400

    Update a few versions to better prep for a release

--- scripts/init.lua
+++ scripts/init.lua
@@ -1,9 +1,9 @@
 local mod = {
   id = "mini_mechs",
   name = "Mini Mechs",
-  version = "1.0.0",
+  version = "1.1.0",
   requirements = {},
-  modApiVersion = "2.5.3",
+  modApiVersion = "2.6.4",
   icon = "img/units/mission/mini_judobot_ns.png"
 }
 
