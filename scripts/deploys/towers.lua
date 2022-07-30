3692e3f4adbc9f27745de48b12f59d81df16ec55
KnightMiner <knightminer4@gmail.com>
2022-07-29 23:26:24 -0400

    Rebalance overdriver
    Overdriver can now only overdrive twice per mission
    Upgrade lets the overdriver overdrive three times per mission
    No longer damages self when overdiriving mechs

--- scripts/deploys/towers.lua
+++ scripts/deploys/towers.lua
@@ -21,7 +21,7 @@ function SquareTargetSkill:GetTargetArea(p1)
       if Board:IsValid(point)
           and (self.TargetSelf or i ~= 0 or j ~= 0)
           and self:IsValidTarget(point) then
-				ret:push_back(point)
+        ret:push_back(point)
       end
     end
   end
@@ -36,18 +36,18 @@ end
 
 --- Unit
 Mini_WindTower = Pawn:new {
-	Name           = "Wind Tower",
-	Health         = 2,
-	MoveSpeed      = 0,
-	DefaultTeam    = TEAM_PLAYER,
-	ImpactMaterial = IMPACT_METAL,
-	SkillList      = { "Mini_WindTorrent" },
-	Pushable       = false,
-	Corpse         = false,
-	-- display
-	Image          = "mini_tower",
+  Name           = "Wind Tower",
+  Health         = 2,
+  MoveSpeed      = 0,
+  DefaultTeam    = TEAM_PLAYER,
+  ImpactMaterial = IMPACT_METAL,
+  SkillList      = { "Mini_WindTorrent" },
+  Pushable       = false,
+  Corpse         = false,
+  -- display
+  Image          = "mini_tower",
   ImageOffset     = modApi:getPaletteImageOffset("Steel Judoka"),
-	SoundLocation  = "/support/earthmover/"
+  SoundLocation  = "/support/earthmover/"
 }
 Mini_WindTowerA  = Mini_WindTower:new { SkillList = { "Mini_WindTorrent_A" } }
 Mini_WindTowerB  = Mini_WindTower:new { SkillList = { "Mini_WindTorrent_B" } }
@@ -58,8 +58,8 @@ Mini_WindTorrent = Support_Wind:new{
   Range    = 2,
   Limited  = 0,
   PushAway = false,
-	Upgrades = 2,
-	UpgradeCost = { 1, 2 },
+  Upgrades = 2,
+  UpgradeCost = { 1, 2 },
 }
 
 Mini_WindTorrent_A  = Mini_WindTorrent:new{
@@ -82,13 +82,13 @@ function Support_Wind:GetTargetArea(point)
   local ret = originalTargetArea(self, point)
 
   if self.PushAway then
-  	ret:push_back(Point(3,3))
-  	ret:push_back(Point(3,4))
-  	ret:push_back(Point(4,3))
-  	ret:push_back(Point(4,4))
+    ret:push_back(Point(3,3))
+    ret:push_back(Point(3,4))
+    ret:push_back(Point(4,3))
+    ret:push_back(Point(4,4))
   end
 
-	return ret
+  return ret
 end
 
 -- mostly a clone of wind torrent, targets different units
@@ -146,27 +146,27 @@ end
 
 -- Deploy
 Mini_DeployWindTower = Deployable:new{
-	Deployed = "Mini_WindTower",
-	Rarity      = 4,
-	PowerCost   = 2,
-	Upgrades    = 2,
-	UpgradeCost = {1, 2},
-	-- visuals
+  Deployed = "Mini_WindTower",
+  Rarity      = 4,
+  PowerCost   = 2,
+  Upgrades    = 2,
+  UpgradeCost = {1, 2},
+  -- visuals
   Icon        = "weapons/deploy_mini_wind_tower.png",
   Projectile  = "effects/shotup_mini_wind_tower.png",
-	LaunchSound = "/weapons/deploy_tank",
-	ImpactSound = "/impact/generic/mech",
+  LaunchSound = "/weapons/deploy_tank",
+  ImpactSound = "/impact/generic/mech",
   TipImage = {
-		Unit          = Point(0,3),
-		Enemy         = Point(2,1),
-		Enemy2        = Point(3,2),
-		Enemy3        = Point(0,2),
-		Mountain      = Point(3,1),
-		Friendly      = Point(1,2),
-		Target        = Point(2,3),
+    Unit          = Point(0,3),
+    Enemy         = Point(2,1),
+    Enemy2        = Point(3,2),
+    Enemy3        = Point(0,2),
+    Mountain      = Point(3,1),
+    Friendly      = Point(1,2),
+    Target        = Point(2,3),
     Second_Origin = Point(2,3),
     Second_Target = Point(3,2)
-	}
+  }
 }
 Mini_DeployWindTower_A  = Mini_DeployWindTower:new{
   Deployed = "Mini_WindTowerA",
@@ -192,18 +192,18 @@ Mini_DeployWindTower_AB = Mini_DeployWindTower:new{ Deployed = "Mini_WindTowerAB
 
 --- Unit
 Mini_Geoformer = Pawn:new {
-	Name           = "Geoformer",
-	Health         = 2,
-	MoveSpeed      = 0,
-	DefaultTeam    = TEAM_PLAYER,
-	ImpactMaterial = IMPACT_METAL,
-	SkillList      = { "Mini_Geoform" },
-	Pushable       = false,
-	Corpse         = false,
-	-- display
-	Image          = "mini_tower",
+  Name           = "Geoformer",
+  Health         = 2,
+  MoveSpeed      = 0,
+  DefaultTeam    = TEAM_PLAYER,
+  ImpactMaterial = IMPACT_METAL,
+  SkillList      = { "Mini_Geoform" },
+  Pushable       = false,
+  Corpse         = false,
+  -- display
+  Image          = "mini_tower",
   ImageOffset     = modApi:getPaletteImageOffset("Flame Behemoths"),
-	SoundLocation  = "/support/earthmover/"
+  SoundLocation  = "/support/earthmover/"
 }
 Mini_GeoformerA  = Mini_Geoformer:new { SkillList = { "Mini_Geoform_A" } }
 Mini_GeoformerB  = Mini_Geoformer:new { SkillList = { "Mini_Geoform_B" } }
@@ -211,24 +211,24 @@ Mini_GeoformerAB = Mini_Geoformer:new { SkillList = { "Mini_Geoform_AB" } }
 
 -- Weapon
 Mini_Geoform = SquareTargetSkill:new{
-	Class       = "Unique",
-	Rarity      = 0,
-	Damage      = 0,
-	Range       = 2,
+  Class       = "Unique",
+  Rarity      = 0,
+  Damage      = 0,
+  Range       = 2,
   UsesPerTurn = 2,
-	PowerCost   = 0,
-	Upgrades    = 2,
-	UpgradeCost = {1, 3},
-	-- display
-	Icon = "weapons/mini_geoform.png",
-	LaunchSound = "/props/lava_tile",
+  PowerCost   = 0,
+  Upgrades    = 2,
+  UpgradeCost = {1, 3},
+  -- display
+  Icon = "weapons/mini_geoform.png",
+  LaunchSound = "/props/lava_tile",
   TipImage = {
-		Unit          = Point(2,3),
-		Hole          = Point(3,1),
+    Unit          = Point(2,3),
+    Hole          = Point(3,1),
     Target        = Point(3,1),
     Second_Origin = Point(2,3),
     Second_Target = Point(2,1)
-	}
+  }
 }
 
 Mini_Geoform_A = Mini_Geoform:new{ UsesPerTurn = 3 }
@@ -247,7 +247,7 @@ function Mini_Geoform:GetSkillEffect(p1, p2)
     ret:AddDamage(SpaceDamage(p2, DAMAGE_DEATH))
   elseif Board:IsSpawning(p2) then
     -- spawns: rock
-  	local damage = SpaceDamage(p2, 0)
+    local damage = SpaceDamage(p2, 0)
     damage.sPawn = "Wall" -- better spawn animation than thrown rock
     ret:AddDamage(damage)
   elseif Board:IsTerrain(p2, TERRAIN_WATER) or Board:IsTerrain(p2, TERRAIN_HOLE) or Board:IsTerrain(p2, TERRAIN_LAVA) then
@@ -259,7 +259,7 @@ function Mini_Geoform:GetSkillEffect(p1, p2)
     ret:AddBounce(p2, -6)
   else
   -- anything else: lava
-  	local damage = SpaceDamage(p2, 0)
+    local damage = SpaceDamage(p2, 0)
     damage.iTerrain = TERRAIN_LAVA
     damage.sImageMark = "combat/icons/mini_lava_icon.png"
     ret:AddDamage(damage)
@@ -273,22 +273,22 @@ end
 
 -- Deploy
 Mini_DeployGeoformer = Deployable:new{
-	Deployed = "Mini_Geoformer",
-	Rarity      = 4,
-	PowerCost   = 2,
-	Upgrades    = 2,
-	UpgradeCost = {1,2},
-	-- visuals
+  Deployed = "Mini_Geoformer",
+  Rarity      = 4,
+  PowerCost   = 2,
+  Upgrades    = 2,
+  UpgradeCost = {1,2},
+  -- visuals
   Icon        = "weapons/deploy_mini_geoformer.png",
   Projectile  = "effects/shotup_mini_geoformer.png",
-	LaunchSound = "/weapons/deploy_tank",
-	ImpactSound = "/impact/generic/mech",
+  LaunchSound = "/weapons/deploy_tank",
+  ImpactSound = "/impact/generic/mech",
   TipImage = {
-		Unit          = Point(2,3),
-		Target        = Point(2,1),
+    Unit          = Point(2,3),
+    Target        = Point(2,1),
     Second_Origin = Point(2,1),
     Second_Target = Point(3,2)
-	}
+  }
 }
 
 Mini_DeployGeoformer_A  = Mini_DeployGeoformer:new{ Deployed = "Mini_GeoformerA" }
@@ -302,18 +302,18 @@ Mini_DeployGeoformer_AB = Mini_DeployGeoformer:new{ Deployed = "Mini_GeoformerAB
 
 --- Unit
 Mini_StormTower = Pawn:new {
-	Name           = "Storm Tower",
-	Health         = 2,
-	MoveSpeed      = 0,
-	DefaultTeam    = TEAM_PLAYER,
-	ImpactMaterial = IMPACT_METAL,
-	SkillList      = { "Mini_LightningStrike" },
-	Pushable       = false,
-	Corpse         = false,
-	-- display
-	Image          = "mini_tower",
+  Name           = "Storm Tower",
+  Health         = 2,
+  MoveSpeed      = 0,
+  DefaultTeam    = TEAM_PLAYER,
+  ImpactMaterial = IMPACT_METAL,
+  SkillList      = { "Mini_LightningStrike" },
+  Pushable       = false,
+  Corpse         = false,
+  -- display
+  Image          = "mini_tower",
   ImageOffset     = modApi:getPaletteImageOffset("Blitzkrieg"),
-	SoundLocation  = "/support/earthmover/"
+  SoundLocation  = "/support/earthmover/"
 }
 Mini_StormTowerA  = Mini_StormTower:new { SkillList = { "Mini_LightningStrike_A" } }
 Mini_StormTowerB  = Mini_StormTower:new { SkillList = { "Mini_LightningStrike_B" } }
@@ -321,20 +321,20 @@ Mini_StormTowerAB = Mini_StormTower:new { SkillList = { "Mini_LightningStrike_AB
 
 -- Weapon
 Mini_LightningStrike = SquareTargetSkill:new{
-	Class       = "Unique",
-	Rarity      = 0,
-	Damage      = 2,
-	Range       = 2,
-	PowerCost   = 0,
-	Upgrades    = 2,
-	UpgradeCost = {2,2},
-	-- display
-	Icon = "weapons/mini_lightning_strike.png",
+  Class       = "Unique",
+  Rarity      = 0,
+  Damage      = 2,
+  Range       = 2,
+  PowerCost   = 0,
+  Upgrades    = 2,
+  UpgradeCost = {2,2},
+  -- display
+  Icon = "weapons/mini_lightning_strike.png",
   TipImage = {
-		Unit          = Point(2,3),
-		Enemy         = Point(2,1),
-		Target        = Point(2,1)
-	}
+    Unit          = Point(2,3),
+    Enemy         = Point(2,1),
+    Target        = Point(2,1)
+  }
 }
 Mini_LightningStrike_A  = Mini_LightningStrike:new{ Damage = 3 }
 Mini_LightningStrike_B  = Mini_LightningStrike:new{ Range = 3 }
@@ -345,8 +345,8 @@ function Mini_LightningStrike:GetSkillEffect(p1, p2)
 
   -- TODO: custom skill tooltip?
   ret:AddSound("/props/lightning_strike")
-	ret:AddDelay(1)
-	local damage = SpaceDamage(p2, self.Damage, DIR_FLIP)
+  ret:AddDelay(1)
+  local damage = SpaceDamage(p2, self.Damage, DIR_FLIP)
   damage.sAnimation = "LightningBolt0"
   ret:AddDamage(damage)
 
@@ -356,23 +356,23 @@ end
 
 -- Deploy
 Mini_DeployStormTower = Deployable:new{
-	Deployed = "Mini_StormTower",
-	Rarity      = 4,
-	PowerCost   = 2,
-	Upgrades    = 2,
-	UpgradeCost = {2,2},
-	-- visuals
+  Deployed = "Mini_StormTower",
+  Rarity      = 4,
+  PowerCost   = 2,
+  Upgrades    = 2,
+  UpgradeCost = {2,2},
+  -- visuals
   Icon        = "weapons/deploy_mini_storm_tower.png",
   Projectile  = "effects/shotup_mini_storm_tower.png",
-	LaunchSound = "/weapons/deploy_tank",
-	ImpactSound = "/impact/generic/mech",
+  LaunchSound = "/weapons/deploy_tank",
+  ImpactSound = "/impact/generic/mech",
   TipImage = {
-		Unit          = Point(2,3),
-		Enemy         = Point(3,2),
-		Target        = Point(2,1),
+    Unit          = Point(2,3),
+    Enemy         = Point(3,2),
+    Target        = Point(2,1),
     Second_Origin = Point(2,1),
     Second_Target = Point(3,2)
-	}
+  }
 }
 Mini_DeployStormTower_A  = Mini_DeployStormTower:new{ Deployed = "Mini_StormTowerA" }
 Mini_DeployStormTower_B  = Mini_DeployStormTower:new{ Deployed = "Mini_StormTowerB" }
@@ -385,18 +385,18 @@ Mini_DeployStormTower_AB = Mini_DeployStormTower:new{ Deployed = "Mini_StormTowe
 
 --- Unit
 Mini_Overdriver = Pawn:new {
-	Name           = "Overdriver",
-	Health         = 2,
-	MoveSpeed      = 0,
-	DefaultTeam    = TEAM_PLAYER,
-	ImpactMaterial = IMPACT_METAL,
-	SkillList      = { "Mini_Overdrive" },
-	Pushable       = false,
-	Corpse         = false,
-	-- display
-	Image          = "mini_tower",
+  Name           = "Overdriver",
+  Health         = 2,
+  MoveSpeed      = 0,
+  DefaultTeam    = TEAM_PLAYER,
+  ImpactMaterial = IMPACT_METAL,
+  SkillList      = { "Mini_Overdrive" },
+  Pushable       = false,
+  Corpse         = false,
+  -- display
+  Image          = "mini_tower",
   ImageOffset     = modApi:getPaletteImageOffset("Secret Squad"),
-	SoundLocation  = "/support/earthmover/"
+  SoundLocation  = "/support/earthmover/"
 }
 Mini_OverdriverA  = Mini_Overdriver:new { SkillList = { "Mini_Overdrive_A" } }
 Mini_OverdriverB  = Mini_Overdriver:new { SkillList = { "Mini_Overdrive_B" } }
@@ -404,23 +404,23 @@ Mini_OverdriverAB = Mini_Overdriver:new { SkillList = { "Mini_Overdrive_AB" } }
 
 -- Weapon
 Mini_Overdrive = SquareTargetSkill:new{
-	Class       = "Unique",
-	Rarity      = 0,
-	Damage      = 2,
-	Range       = 2,
-	PowerCost   = 0,
-	SelfDamage  = 1,
-	Upgrades    = 2,
-	UpgradeCost = {2,2},
-	-- display
-	Icon = "weapons/mini_overdrive.png",
+  Class       = "Unique",
+  Rarity      = 0,
+  Damage      = 0,
+  Range       = 2,
+  PowerCost   = 0,
+  Limited     = 2,
+  Upgrades    = 2,
+  UpgradeCost = {2,2},
+  -- display
+  Icon = "weapons/mini_overdrive.png",
   TipImage = {
-		Unit          = Point(2,3),
-		Friendly      = Point(2,1),
+    Unit          = Point(2,3),
+    Friendly      = Point(2,1),
     Target        = Point(2,1)
-	}
+  }
 }
-Mini_Overdrive_A = Mini_Overdrive:new{ SelfDamage = 0 }
+Mini_Overdrive_A = Mini_Overdrive:new{ Limited = 3 }
 Mini_Overdrive_B = Mini_Overdrive:new{ Range = 3 }
 Mini_Overdrive_AB = Mini_Overdrive_A:new{ Range = 3 }
 
@@ -438,15 +438,15 @@ function Mini_Overdrive:GetSkillEffect(p1, p2)
 
   -- power on unit again
   ret:AddSound("/enemy/shared/robot_power_on")
-	ret:AddScript(string.format([[
-		local p = %s
+  ret:AddScript(string.format([[
+    local p = %s
     local unit = Board:GetPawn(p)
-		if unit then
-			unit:SetActive(true)
-			Board:Ping(p, GL_Color(128, 0, 255))
-		end]],
-		p2:GetString()
-	))
+    if unit then
+      unit:SetActive(true)
+      Board:Ping(p, GL_Color(128, 0, 255))
+    end]],
+    p2:GetString()
+  ))
 
   local icon = SpaceDamage(p2, 0)
   icon.sImageMark = "combat/icons/mini_overdrive_icon.png"
@@ -454,8 +454,8 @@ function Mini_Overdrive:GetSkillEffect(p1, p2)
 
   -- self damgae when activating mechs
   local target = Board:GetPawn(p2)
-  if target and target:IsMech() and self.SelfDamage > 0 then
-    ret:AddDamage(SpaceDamage(p1, self.SelfDamage))
+  if target and target:IsMech() and self.Damage > 0 then
+    ret:AddDamage(SpaceDamage(target, self.Damage))
   end
 
   return ret
@@ -463,24 +463,34 @@ end
 
 -- Deploy
 Mini_DeployOverdriver = Deployable:new{
-	Deployed = "Mini_Overdriver",
-	Rarity      = 4,
-	PowerCost   = 2,
-	Upgrades    = 2,
-	UpgradeCost = {2,2},
-	-- visuals
+  Deployed = "Mini_Overdriver",
+  Rarity      = 4,
+  PowerCost   = 2,
+  Upgrades    = 2,
+  UpgradeCost = {2,2},
+  -- visuals
   Icon        = "weapons/deploy_mini_overdriver.png",
   Projectile  = "effects/shotup_mini_overdriver.png",
-	LaunchSound = "/weapons/deploy_tank",
-	ImpactSound = "/impact/generic/mech",
+  LaunchSound = "/weapons/deploy_tank",
+  ImpactSound = "/impact/generic/mech",
   TipImage = {
-		Unit          = Point(2,3),
-		Friendly      = Point(1,2),
-		Target        = Point(2,1),
+    Unit          = Point(2,3),
+    Friendly      = Point(1,2),
+    Target        = Point(2,1),
     Second_Origin = Point(2,1),
     Second_Target = Point(1,2)
-	}
+  }
 }
 Mini_DeployOverdriver_A = Mini_DeployOverdriver:new{ Deployed = "Mini_OverdriverA" }
 Mini_DeployOverdriver_B = Mini_DeployOverdriver:new{ Deployed = "Mini_OverdriverB" }
 Mini_DeployOverdriver_AB = Mini_DeployOverdriver:new{ Deployed = "Mini_OverdriverAB" }
+
+function Mini_DeployOverdriver:GetSkillEffect(p1, p2)
+  local ret = SkillEffect()
+  local damage = SpaceDamage(p2,0)
+  damage.sPawn = self.Deployed
+  ret:AddArtillery(damage,self.Projectile)
+  -- for some reason the overdriver spawns with only 1 use remaining despite having 2, so just reset it to the full amount
+  ret:AddScript("Board:GetPawn("..p2:GetString().."):ResetUses()")
+  return ret
+end
